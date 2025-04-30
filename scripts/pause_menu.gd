9845b24e9e7a991e94927916d8a8f870dcec185e
Mayank Meena <dcryptoniun@gmail.com>
2025-04-30 18:14:43 +0530

    feat(ads): integrate Admob for interstitial ads in pause menu
    Add Admob initialization and interstitial ad loading/showing logic to the pause menu. This allows displaying ads when the user navigates to the main menu, enhancing monetization opportunities.

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,10 +1,14 @@
 extends CanvasLayer
 
+@onready var admob: Admob = $Admob as Admob
+
+var _is_interstitial_loaded: bool = false
 # This script handles the pause menu functionality
 
 func _ready():
 	# Make sure the pause menu is hidden when the game starts
 	visible = false
+	admob.initialize()
 
 # Resume button handler
 func _on_resume_button_pressed():
@@ -22,6 +26,11 @@ func _on_restart_button_pressed():
 
 # Main menu button handler
 func _on_main_menu_button_pressed():
+	if _is_interstitial_loaded:
+		_is_interstitial_loaded = false
+		admob.show_interstitial_ad()
+	else:
+		admob.load_interstitial_ad()
 	# Resume the game before changing scenes
 	get_tree().paused = false
 	
@@ -40,3 +49,11 @@ func _on_main_menu_button_pressed():
 	# Remove the old scene
 	get_tree().root.remove_child(current_scene)
 	current_scene.queue_free()
+
+
+func _on_admob_initialization_completed(status_data: InitializationStatus) -> void:
+	admob.load_interstitial_ad()
+
+
+func _on_admob_interstitial_ad_loaded(ad_id: String) -> void:
+	_is_interstitial_loaded = true
